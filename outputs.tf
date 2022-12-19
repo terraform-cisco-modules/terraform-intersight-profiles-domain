d154e890934279eefbfc9bb85ecd880eb6509f34
scotttyso <tyscott@cisco.com>
2022-12-18 22:27:19 -0500

    changing domains output

--- outputs.tf
+++ outputs.tf
@@ -5,6 +5,12 @@
 
 output "domains" {
   value = {
+    for v in sort(keys(intersight_fabric_switch_cluster_profile.domain_profile)
+    ) : v => intersight_fabric_switch_cluster_profile.domain_profile[v].moid
+  }
+}
+output "switch_profiles" {
+  value = {
     for v in sort(keys(intersight_fabric_switch_profile.switch_profiles)) : v => merge({
       moid = intersight_fabric_switch_profile.switch_profiles[v].moid
     }, local.switch_profiles[v])
@@ -24,11 +30,11 @@ output "domains" {
 #  }
 #}
 
-output "depoy_switch_profiles" {
-  value = {
-    for v in sort(keys(intersight_fabric_switch_profile.switch_profiles)
-      ) : v => intersight_fabric_switch_profile.switch_profiles[v].moid if length(regexall(
-      "^[A-Z]{3}[2-3][\\d]([0][1-9]|[1-4][0-9]|[5][1-3])[\\dA-Z]{4}$", local.switch_profiles[v].serial_number)
-    ) > 0 && local.switch_profiles[v].action == "Deploy"
-  }
-}
+#output "depoy_switch_profiles" {
+#  value = {
+#    for v in sort(keys(intersight_fabric_switch_profile.switch_profiles)
+#      ) : v => intersight_fabric_switch_profile.switch_profiles[v].moid if length(regexall(
+#      "^[A-Z]{3}[2-3][\\d]([0][1-9]|[1-4][0-9]|[5][1-3])[\\dA-Z]{4}$", local.switch_profiles[v].serial_number)
+#    ) > 0 && local.switch_profiles[v].action == "Deploy"
+#  }
+#}
